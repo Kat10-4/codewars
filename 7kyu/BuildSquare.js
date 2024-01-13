ea1795e912690dc671f214bed83bcd45b03db91e
Kateryna Diachenko <143129834+Kat10-4@users.noreply.github.com>
2024-01-13 19:43:28 +0200

    BuildSquare

---
+++ 7kyu/BuildSquare.js
@@ -0,0 +1,21 @@
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(integer) {
+  let string = "";
+  for (let i = 0; i < integer; i++) {
+    if (i > 0) {
+      string += "\n";
+    }
+    for (let j = 0; j < integer; j++) {
+      string += "+";
+    }
+  }
+  return string;
+}
