e2e71dc3c8c36d04dfda14bd0e3a03df10105fb1
Kateryna Diachenko <143129834+Kat10-4@users.noreply.github.com>
2024-01-29 21:29:08 +0200

    isIntegerArray

---
+++ 6kyu/isIntegerArray.js
@@ -0,0 +1,16 @@
+// Write a function with the signature shown below:
+
+// function isIntArray(arr) {
+//   return true
+// }
+// returns true  / True if every element in an array is an integer or a float with no decimals.
+// returns true  / True if array is empty.
+// returns false / False for every other input.
+
+function isIntArray(arr) {
+  return arr !== null && arr !== undefined && Array.isArray(arr)
+    ? arr.every((el) => el % 1 === 0 && typeof el === "number")
+      ? true
+      : false
+    : false;
+}
